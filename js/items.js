ee2d7831c64db574a59dce723fc44513a387ef71
unknown <rock0419@moj.ad>
2019-07-23 10:28:47 +0800

    20190723

--- js/items.js
+++ js/items.js
@@ -1,5 +1,24 @@
-var auction_items = [
-item_number,item_name,unit,quantity,price,item_description,num_images,num_videos,auction_type
-1,LOUIS VUITTON鞋,雙,1,"5,000",尺寸36,1,0,變賣
-2,LOUIS VUITTON鞋,雙,1,"2,000",尺寸36,1,0,變賣
-];
+var auction_items =[
+    {
+      "item_number": 1,
+      "item_name": "LOUIS VUITTON鞋",
+      "unit": "雙",
+      "quantity": 1,
+      "price": "5,000",
+      "item_description": "尺寸36",
+      "num_images": 1,
+      "num_videos": 0,
+      "auction_type": "變賣"
+    },
+    {
+      "item_number": 2,
+      "item_name": "LOUIS VUITTON鞋",
+      "unit": "雙",
+      "quantity": 1,
+      "price": "2,000",
+      "item_description": "尺寸36",
+      "num_images": 1,
+      "num_videos": 0,
+      "auction_type": "變賣"
+    }
+  ]
